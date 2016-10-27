c1e978d40d271986d1a737c016b1ed92d5cb1d8c
Cherlynn\Cherlynn <cherlynn@live.com.sg>
2016-10-27 21:40:22 +0800

    extract methods CommandHelper

--- src/main/java/teamfour/tasc/logic/commands/CommandHelper.java
+++ src/main/java/teamfour/tasc/logic/commands/CommandHelper.java
@@ -15,10 +15,9 @@ import teamfour.tasc.logic.parser.KeywordParser;
 import teamfour.tasc.model.task.Recurrence;
 
 public class CommandHelper {
-    private static final String MESSAGE_INVALID_DATES = "Invalid dates";
-
     private static final Logger logger = LogsCenter.getLogger(CommandHelper.class);
 
+    private static final String MESSAGE_INVALID_DATES = "Invalid dates";
     public static String MESSAGE_REPEAT_PARAMETERS_INVALID = "Invalid repeat parameters";
 
     //@@author A0148096W
@@ -80,11 +79,12 @@ public class CommandHelper {
      * Dates parsed include time of day, day of week, day of month, month of year, and year
      * Time of day can be in numerical 12 hours or 24 hours or word form (7pm or 1900 hours or seven in the evening)
      * Day of week can be in short or full (fri or friday)
-     * Day of month can be in numerical or english word form (13 or thirteenth)
+     * Day of month can be in numerical or word form (13 or thirteenth)
      * Month of year can only be in word form, and can be in short(january or jan, but not 1)
      * Year must be in numerical form, and put after month and day
-     * @param dateInString
-     * @return List of dates parsed from dateInString
+     *
+     * @param dateInString  Input String containing the date(s)
+     * @return dates    List of dates parsed from dateInString
      */
     public static List<Date> convertStringToMultipleDates(String dateInString){
         if(dateInString == null){
@@ -100,7 +100,7 @@ public class CommandHelper {
      * If date given is today, and no time is set/time set is in the past, then time will be set to 11.59pm
      * @param dateInString
      * @return Date parsed from dateInString
-     * @throws Exception
+     * @throws IllegalValueException
      */
     public static Date convertStringToDate(String dateInString) throws IllegalValueException{
         dateInString = convertStringIfTodayAndNoValidTime(dateInString);
@@ -114,22 +114,32 @@ public class CommandHelper {
     /**
      * Converts dateInString to "today 11.59pm" if it contains "today",
      * and no time is given or time given is in the past
-     * @param dateInString
-     * @return dateInString
+     * @param dateInString  String containing the date
+     * @return dateInStringConverted    String containing the date, but converted accordingly
      * @throws IllegalValueException
      */
     private static String convertStringIfTodayAndNoValidTime(String dateInString) throws IllegalValueException {
+        String dateInStringConverted = dateInString;
         if(dateInString.toLowerCase().contains("today")){
             List<Date> dates = new PrettyTimeParser().parse(dateInString);
             if(dates.size() != 1){
                 throw new IllegalValueException(MESSAGE_INVALID_DATES);
             }
-            if ((new Date().getDate() == dates.get(0).getDate())
-                    && (new Date().after(dates.get(0)) || new Date().equals(dates.get(0)))) {
-                dateInString = "today 11.59pm";
+            if (isTimeParsedEarlierThanOrEqualToNow(dates.get(0))) {
+                dateInStringConverted = "today 11.59pm";
             }
         }
-        return dateInString;
+        return dateInStringConverted;
+    }
+
+    /**
+     * Returns true if date is on the same day as today, but at earlier/same time. Returns false otherwise
+     * @param date  The Date parsed by PrettyTimeParser
+     * @return true or false
+     */
+    private static boolean isTimeParsedEarlierThanOrEqualToNow(Date date) {
+        return (new Date().getDate() == date.getDate())
+                && (new Date().after(date) || new Date().equals(date));
     }
 
     /**
