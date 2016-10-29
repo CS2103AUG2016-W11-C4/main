085d52a6b91e61272dab12c2cf79ba16b7899f7e
Wang Leng Tan <tanwangleng@outlook.com>
2016-10-29 12:55:24 +0800

    Force Coveralls to recognize our keyword tests

--- src/test/java/teamfour/tasc/logic/keyword/KeywordTest.java
+++ src/test/java/teamfour/tasc/logic/keyword/KeywordTest.java
@@ -8,36 +8,41 @@ public class KeywordTest {
 
     @Test
     public void keyword_keywordObjects_allKeywordsMatchWhatUserWillUse() {
-        assertEquals("add", AddCommandKeyword.keyword);
-        assertEquals("by", ByKeyword.keyword);
-        assertEquals("calendar", CalendarCommandKeyword.keyword);
-        assertEquals("clear", ClearCommandKeyword.keyword);
-        assertEquals("collapse", CollapseCommandKeyword.keyword);
-        assertEquals("complete", CompleteCommandKeyword.keyword);
-        assertEquals("day", DayKeyword.keyword);
-        assertEquals("delete", DeleteCommandKeyword.keyword);
-        assertEquals("exit", ExitCommandKeyword.keyword);
-        assertEquals("expand", ExpandCommandKeyword.keyword);
-        assertEquals("find", FindCommandKeyword.keyword);
-        assertEquals("from", FromKeyword.keyword);
-        assertEquals("help", HelpCommandKeyword.keyword);
-        assertEquals("hide", HideCommandKeyword.keyword);
-        assertEquals("list", ListCommandKeyword.keyword);
-        assertEquals("on", OnKeyword.keyword);
-        assertEquals("redo", RedoCommandKeyword.keyword);
-        assertEquals("relocate", RelocateCommandKeyword.keyword);
-        assertEquals("renamelist", RenameListCommandKeyword.keyword);
-        assertEquals("repeat", RepeatKeyword.keyword);
-        assertEquals("select", SelectCommandKeyword.keyword);
-        assertEquals("show", ShowCommandKeyword.keyword);
-        assertEquals("sort", SortKeyword.keyword);
-        assertEquals("switchlist", SwitchListCommandKeyword.keyword);
-        assertEquals("tag", TagKeyword.keyword);
-        assertEquals("today", TodayKeyword.keyword);
-        assertEquals("to", ToKeyword.keyword);
-        assertEquals("undo", UndoCommandKeyword.keyword);
-        assertEquals("update", UpdateCommandKeyword.keyword);
-        assertEquals("week", WeekKeyword.keyword);
+        
+        // If we don't create Keyword objects, (even though
+        // they aren't necessary due to the statics), Coveralls will
+        // complain that we never test the classes :(
+        
+        assertEquals("add", new AddCommandKeyword().keyword);
+        assertEquals("by", new ByKeyword().keyword);
+        assertEquals("calendar", new CalendarCommandKeyword().keyword);
+        assertEquals("clear", new ClearCommandKeyword().keyword);
+        assertEquals("collapse", new CollapseCommandKeyword().keyword);
+        assertEquals("complete", new CompleteCommandKeyword().keyword);
+        assertEquals("day", new DayKeyword().keyword);
+        assertEquals("delete", new DeleteCommandKeyword().keyword);
+        assertEquals("exit", new ExitCommandKeyword().keyword);
+        assertEquals("expand", new ExpandCommandKeyword().keyword);
+        assertEquals("find", new FindCommandKeyword().keyword);
+        assertEquals("from", new FromKeyword().keyword);
+        assertEquals("help", new HelpCommandKeyword().keyword);
+        assertEquals("hide", new HideCommandKeyword().keyword);
+        assertEquals("list", new ListCommandKeyword().keyword);
+        assertEquals("on", new OnKeyword().keyword);
+        assertEquals("redo", new RedoCommandKeyword().keyword);
+        assertEquals("relocate", new RelocateCommandKeyword().keyword);
+        assertEquals("renamelist", new RenameListCommandKeyword().keyword);
+        assertEquals("repeat", new RepeatKeyword().keyword);
+        assertEquals("select", new SelectCommandKeyword().keyword);
+        assertEquals("show", new ShowCommandKeyword().keyword);
+        assertEquals("sort", new SortKeyword().keyword);
+        assertEquals("switchlist", new SwitchListCommandKeyword().keyword);
+        assertEquals("tag", new TagKeyword().keyword);
+        assertEquals("today", new TodayKeyword().keyword);
+        assertEquals("to", new ToKeyword().keyword);
+        assertEquals("undo", new UndoCommandKeyword().keyword);
+        assertEquals("update", new UpdateCommandKeyword().keyword);
+        assertEquals("week", new WeekKeyword().keyword);
     }
 
 }
