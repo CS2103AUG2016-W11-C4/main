fc71ad03bda988ff80f5610a6095e68e6e61c7fc
Wang Leng Tan <tanwangleng@outlook.com>
2016-10-30 15:37:37 +0800

    Rename the variables used in test cases to better names

--- src/test/java/teamfour/tasc/model/task/DeadlineTest.java
+++ src/test/java/teamfour/tasc/model/task/DeadlineTest.java
@@ -5,48 +5,48 @@ import static org.junit.Assert.*;
 
 import java.util.Date;
 
+import org.junit.Before;
 import org.junit.Test;
 
 import teamfour.tasc.model.task.Deadline;
 
 public class DeadlineTest {
+    
+    private Deadline noDeadline;
+    private Date epochDate;
+    private Deadline epochDeadline;
+    
+    @Before
+    public void setUp() {
+        noDeadline = new Deadline();
+        epochDate = new Date(0);
+        epochDeadline = new Deadline(epochDate);
+    }
 
     @Test
-    public void defaultConstructor_noInput_returnsEmptyDeadline() {
-        Deadline noDeadline = new Deadline();
-        
+    public void defaultConstructor_noInput_returnsEmptyDeadline() {        
         assertFalse(noDeadline.hasDeadline());
         assertNull(noDeadline.getDeadline());
     }
     
     @Test
-    public void constructor_validDateGiven_returnsDeadlineWithDate() {
-        Date date = new Date();
-        Deadline deadline = new Deadline(date);
-        
-        assertTrue(deadline.hasDeadline());
-        assertEquals(date, deadline.getDeadline());
+    public void constructor_validDateGiven_returnsDeadlineWithDate() {        
+        assertTrue(epochDeadline.hasDeadline());
+        assertEquals(epochDate, epochDeadline.getDeadline());
     }
     
     @Test
-    public void toString_noDeadline_returnsNoDeadline() {
-        Deadline noDeadline = new Deadline();
-        
+    public void toString_noDeadline_returnsNoDeadline() {        
         assertEquals(Deadline.TO_STRING_NO_DEADLINE, noDeadline.toString());
     }
     
     @Test
-    public void toString_validDeadline_returnsSameDate() {
-        Date date = new Date();
-        Deadline deadline = new Deadline(date);
-        
-        assertEquals(date.toString(), deadline.toString());   
+    public void toString_validDeadline_returnsSameDate() {        
+        assertEquals(epochDate.toString(), epochDeadline.toString());   
     }
     
     @Test
     public void equals_nonDeadlineObject_returnsFalse() {
-        Deadline noDeadline = new Deadline();
-        
         assertFalse(noDeadline.equals("String"));
     }
 }
--- src/test/java/teamfour/tasc/model/task/PeriodTest.java
+++ src/test/java/teamfour/tasc/model/task/PeriodTest.java
@@ -5,6 +5,7 @@ import static org.junit.Assert.*;
 
 import java.util.Date;
 
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.experimental.categories.Categories.ExcludeCategory;
 
@@ -12,6 +13,15 @@ import teamfour.tasc.commons.exceptions.IllegalValueException;
 import teamfour.tasc.model.task.Period;
 
 public class PeriodTest {
+
+    private Date validStartTime;
+    private Date validEndTime;
+    
+    @Before
+    public void setUp() {
+        validStartTime = new Date(5);
+        validEndTime = new Date(10);
+    }
 
     @Test
     public void defaultConstructor_noInput_returnsEmptyPeriod() {
@@ -24,14 +34,11 @@ public class PeriodTest {
 
     @Test
     public void constructor_validStartEndTime_returnsCorrectPeriod() throws IllegalValueException {
-        Date startTime = new Date(5);
-        Date endTime = new Date(10);
+        Period validPeriod = new Period(validStartTime, validEndTime);
         
-        Period period = new Period(startTime, endTime);
-        
-        assertTrue(period.hasPeriod());
-        assertEquals(startTime, period.getStartTime());
-        assertEquals(endTime, period.getEndTime());
+        assertTrue(validPeriod.hasPeriod());
+        assertEquals(validStartTime, validPeriod.getStartTime());
+        assertEquals(validEndTime, validPeriod.getEndTime());
     }
     
     @Test (expected = IllegalValueException.class)
@@ -39,7 +46,7 @@ public class PeriodTest {
         Date earlierTime = new Date(5);
         Date laterTime = new Date(10);
         
-        Period period = new Period(laterTime, earlierTime);
+        Period invalidPeriod = new Period(laterTime, earlierTime);
     }
     
     @Test
@@ -50,13 +57,11 @@ public class PeriodTest {
     
     @Test
     public void toString_validPeriod_returnsValidTime() throws IllegalValueException {
-        Date earlierTime = new Date(0);
-        Date laterTime = new Date(10);
-        Period period = new Period(earlierTime, laterTime);
+        Period validPeriod = new Period(validStartTime, validEndTime);
         
-        String expectedOutput = earlierTime.toString() + " - " + laterTime.toString();
+        String expectedOutput = validStartTime.toString() + " - " + validEndTime.toString();
         
-        assertEquals(expectedOutput, period.toString());
+        assertEquals(expectedOutput, validPeriod.toString());
     }
     
     @Test
