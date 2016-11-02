dbd7de392f490b0eaf31edf5b062152841f6c206
Cherlynn\Cherlynn <cherlynn@live.com.sg>
2016-11-02 20:15:11 +0800

    add test cases for invalid filenames

--- src/test/java/guitests/SwitchlistCommandTest.java
+++ src/test/java/guitests/SwitchlistCommandTest.java
@@ -1,14 +1,29 @@
 package guitests;
 
 import static org.junit.Assert.*;
+import static teamfour.tasc.commons.core.Messages.MESSAGE_INVALID_COMMAND_FORMAT;
 
 import org.junit.Test;
+
+import teamfour.tasc.logic.commands.SwitchlistCommand;
 
 public class SwitchlistCommandTest extends AddressBookGuiTest{
 
     @Test
-    public void test() {
-        fail("Not yet implemented");
-    }
+    public void renamelist_invalidInput_failure() {
 
+        String testFilename = "invalidNameWithSymbols@#$";
+        commandBox.runCommand("switchlist " + testFilename);
+        assertResultMessage(String.format(MESSAGE_INVALID_COMMAND_FORMAT, SwitchlistCommand.MESSAGE_USAGE));
+
+        //empty name
+        testFilename = "";
+        commandBox.runCommand("switchlist " + testFilename);
+        assertResultMessage(String.format(MESSAGE_INVALID_COMMAND_FORMAT, SwitchlistCommand.MESSAGE_USAGE));
+
+        //name with only spaces
+        testFilename = "       ";
+        commandBox.runCommand("switchlist " + testFilename);
+        assertResultMessage(String.format(MESSAGE_INVALID_COMMAND_FORMAT, SwitchlistCommand.MESSAGE_USAGE));
+    }
 }
