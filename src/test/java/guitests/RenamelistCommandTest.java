ea2eee1699a1bd73d2469fca58fd4b70f9583db5
Cherlynn\Cherlynn <cherlynn@live.com.sg>
2016-11-02 19:52:46 +0800

    add tests for renamelist - valid inputs

--- src/test/java/guitests/RenamelistCommandTest.java
+++ src/test/java/guitests/RenamelistCommandTest.java
@@ -1,14 +1,29 @@
 package guitests;
 
 import static org.junit.Assert.*;
+import static teamfour.tasc.commons.core.Messages.MESSAGE_INVALID_COMMAND_FORMAT;
 
 import org.junit.Test;
 
-public class RenamelistCommandTest {
+import teamfour.tasc.logic.commands.RenamelistCommand;
+
+public class RenamelistCommandTest extends AddressBookGuiTest{
 
     @Test
-    public void test() {
-        fail("Not yet implemented");
+    public void renamelist_validInput_success() {
+
+        String testFilename = "validName";
+        commandBox.runCommand("renamelist " + testFilename);
+        assertResultMessage(String.format(RenamelistCommand.MESSAGE_SUCCESS, testFilename));
+
+        testFilename = "validNameWithNumbers123";
+        commandBox.runCommand("renamelist " + testFilename);
+        assertResultMessage(String.format(RenamelistCommand.MESSAGE_SUCCESS, testFilename));
+
+        testFilename = "valid name with spaces and numbers";
+        commandBox.runCommand("renamelist " + testFilename);
+        assertResultMessage(String.format(RenamelistCommand.MESSAGE_SUCCESS, testFilename));
+
     }
 
 }
